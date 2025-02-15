6f4dbad9c27cd3bd0d61f7f59a539b5ab82c0ad6
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2025-02-16 04:25:10 +0900

    Add additional skill icons and update layout for Skill component

--- src/components/Skill/Skill.tsx
+++ src/components/Skill/Skill.tsx
@@ -2,6 +2,15 @@ import Style from '@styles/componentsStyles/Skill.module.scss';
 import ESlint from '../icons/skillIcon/ESlint';
 import JavaScript from '../icons/skillIcon/JavaScript';
 import TypeScript from '../icons/skillIcon/TypeScript';
+import React from '../icons/skillIcon/React';
+import Next from '../icons/skillIcon/Next';
+import Node from '../icons/skillIcon/Node';
+import Express from '../icons/skillIcon/Express';
+import SocketIo from '../icons/skillIcon/SocketIo';
+import MySQL from '../icons/skillIcon/MySQL';
+import HTML from '../icons/skillIcon/HTML';
+import Prettier from '../icons/skillIcon/Prettier';
+import Git from '../icons/skillIcon/Git';
 
 export default function Skill() {
     return (
@@ -18,6 +27,46 @@ export default function Skill() {
                 <div className={Style.skillItem}>
                     <TypeScript />
                     <p>TypeScript</p>
+                </div>
+                <div className={Style.skillItem}>
+                    <React />
+                    <p>React</p>
+                </div>
+                <div className={Style.skillItem}>
+                    <Next />
+                    <p>Next.js</p>
+                </div>
+                <div className={Style.skillItem}>
+                    <Node />
+                    <p>Node.js</p>
+                </div>
+                <div className={Style.skillItem}>
+                    <Express />
+                    <p>Express.js</p>
+                </div>
+                <div className={Style.skillItem}>
+                    <SocketIo />
+                    <p>Socket.io</p>
+                </div>
+                <div className={Style.skillItem}>
+                    <MySQL />
+                    <p>MySQL</p>
+                </div>
+                <div className={Style.skillItem}>
+                    <HTML />
+                    <p>HTML5</p>
+                </div>
+                <div className={Style.skillItem}>
+                    <Git />
+                    <p>Git</p>
+                </div>
+                <div className={Style.skillItem}>
+                    <ESlint />
+                    <p>ESlint</p>
+                </div>
+                <div className={Style.skillItem}>
+                    <Prettier />
+                    <p>Prettier</p>
                 </div>
             </div>
         </div>
