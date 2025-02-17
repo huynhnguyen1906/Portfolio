d7dc4862e3e0c8f4043720a054e9688378b630b2
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2025-02-17 11:52:55 +0900

    Add metadata generation for improved SEO and social sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,48 @@
 import { Inter } from 'next/font/google';
 import '@styles/GlobalStyles.scss';
 
+import { Metadata } from 'next';
+
+export const generateMetadata = async (): Promise<Metadata> => {
+    return {
+        title: 'HUYNHs Portfolio',
+        description:
+            'こんにちは、グエン ドゥック フィン（Nguyễn Đức Huỳnh）と申します。ECCコンピュータ専門学校の2年次を修了しました。コンピューターに興味があり、特にWeb開発に関心を持っています。将来はフルスタックエンジニアとして活躍したいと考えています。',
+        applicationName: '旅ナビ',
+        openGraph: {
+            type: 'website',
+            url: `https://nguyenduchuynh.com/`,
+            title: 'HUYNHs Portfolio',
+            description:
+                'こんにちは、グエン ドゥック フィン（Nguyễn Đức Huỳnh）と申します。ECCコンピュータ専門学校の2年次を修了しました。コンピューターに興味があり、特にWeb開発に関心を持っています。将来はフルスタックエンジニアとして活躍したいと考えています。',
+            images: [
+                {
+                    url: 'https://raw.githubusercontent.com/huynhnguyen1906/Portfolio/refs/heads/main/public/images/HeroImage.webp',
+                    width: 300,
+                    height: 300,
+                    alt: 'こんにちは、グエン ドゥック フィン（Nguyễn Đức Huỳnh）と申します。ECCコンピュータ専門学校の2年次を修了しました。コンピューターに興味があり、特にWeb開発に関心を持っています。将来はフルスタックエンジニアとして活躍したいと考えています。',
+                },
+            ],
+            siteName: '歴てく',
+            locale: 'ja_JP',
+        },
+        twitter: {
+            card: 'summary_large_image',
+            site: '',
+            creator: '@Huynhnguyen1906',
+            title: 'HUYNHs Portfolio',
+            description:
+                'こんにちは、グエン ドゥック フィン（Nguyễn Đức Huỳnh）と申します。ECCコンピュータ専門学校の2年次を修了しました。コンピューターに興味があり、特にWeb開発に関心を持っています。将来はフルスタックエンジニアとして活躍したいと考えています。',
+            images: 'https://raw.githubusercontent.com/huynhnguyen1906/Portfolio/refs/heads/main/public/images/HeroImage.webp',
+        },
+        appleWebApp: {
+            capable: true,
+            title: 'HUYNHs Portfolio',
+            statusBarStyle: 'default',
+        },
+    };
+};
+
 const inter = Inter({
     subsets: ['latin'],
     weight: ['100', '300', '400', '500', '600', '700', '800', '900'],
