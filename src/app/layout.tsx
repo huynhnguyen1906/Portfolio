e2756bde0d164f34e47710c4a430a2b57f296919
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2025-02-17 11:58:29 +0900

    Reduce image dimensions in metadata for improved loading performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,8 @@ export const generateMetadata = async (): Promise<Metadata> => {
             images: [
                 {
                     url: 'https://raw.githubusercontent.com/huynhnguyen1906/Portfolio/refs/heads/main/public/images/HeroImage.webp',
-                    width: 150,
-                    height: 150,
+                    width: 50,
+                    height: 50,
                     alt: 'こんにちは、グエン ドゥック フィン（Nguyễn Đức Huỳnh）と申します。ECCコンピュータ専門学校の2年次を修了しました。コンピューターに興味があり、特にWeb開発に関心を持っています。将来はフルスタックエンジニアとして活躍したいと考えています。',
                 },
             ],
